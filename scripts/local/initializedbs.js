0823fc2aad42b030192786b1a2f66973980a5144
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-10-11 06:45:04 -0400

    Adjusting docker-compose for initialize scripts

---
+++ scripts/local/initializedbs.js
@@ -0,0 +1,10 @@
+db = db.getSiblingDB('admin');
+db.createUser({ user: "openrmf" , pwd: "REDACTED", roles: ["readWriteAnyDatabase"]});
+db.createUser({ user: "openrmftemplate" , pwd: "REDACTED", roles: ["readWriteAnyDatabase"]});
+db.createUser({ user: "openrmfscore" , pwd: "REDACTED", roles: ["readWriteAnyDatabase"]});
+db = db.getSiblingDB('openrmf');
+db.createCollection("Artifacts");
+db = db.getSiblingDB('openrmftemplate');
+db.createCollection("Templates");
+db = db.getSiblingDB('openrmfscore');
+db.createCollection("Scores");
